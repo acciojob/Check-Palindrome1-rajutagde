97ee8ae2871467229de0f195085b25f18aa8c9a4
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-03-15 22:11:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// complete the given function
-function isPalindrome(s) {
-  // Convert to lowercase and remove non-alphanumeric characters
-  s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-  
-  // Compare with reversed string
-  return s === s.split('').reverse().join('');
-}
+//
