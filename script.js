b56b7b75741abc5aebe2ad37b7b53444b0859549
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-03-18 15:30:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-// complete the given function
-
 function palindrome(str){
- str.toLowerCase();
- let verdict  = true;
-	for(let i = 0;i<str.length;i++){ 		for( let j = str.length;j>0;j--){
-	if(String[i]!==String[j]){
- 				verdict = false;
- 			}
- 		}
- 	}
- 	return verdict;
- }
- const string = str.toLowerCase();
- const len = str.length;
+    const string = str.toLowerCase();
+    const len = string.length;
 
     // loop through half of the string
     for (let i = 0; i < len / 2; i++) {
 
-        // check if first and last string are same
+        // check if first and last characters are same
         if (string[i] !== string[len - 1 - i]) {
             return false;
         }
